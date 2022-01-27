dd788c94183156b026b269a071236a6671a33bce
P0SlX <25037788+P0SlX@users.noreply.github.com>
2022-01-27 19:51:23 +0100

    [FIX] Crash message vide log

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -2,9 +2,12 @@ module.exports = {
     name: 'messageDelete',
     once: false,
     async execute(message) {
+        if (message.content == null) return;
+        if (message.content.length === 0 ) return;
 
         // Serveur de dev
         if (message.guild.id.toString() === "688788377092358212") return;
+        if (message.content === "admin") return;
 
         const db = global.DB;
         db.serialize(() => {
