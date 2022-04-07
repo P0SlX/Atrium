ed9996d112f6c995eab70cc5e99836597332ead3
Maetis <41517613+Maetis0511@users.noreply.github.com>
2022-04-07 18:45:10 +0200

    Ajout de la commande /profileadd pour ajouter des comptes à enregistrer

---
+++ commands/profileadd.js
@@ -0,0 +1,96 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const mysql = require('mysql');
+const { MessageEmbed } = require('discord.js');
+// eslint-disable-next-line no-shadow
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('profileadd')
+		.setDescription('Ajouter un profile lol à regarder')
+		.addStringOption(option => option.setName('pseudo').setDescription('Profile à ajouter').setRequired(true)),
+
+	async execute(interaction) {
+
+		let param;
+		if (/\s/.test(interaction.options.getString('pseudo'))) {
+			param = encodeURIComponent(interaction.options.getString('pseudo').trim());
+		}
+		else {
+			param = interaction.options.getString('pseudo');
+		}
+
+		let sum = undefined;
+		await fetch('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + param + '?api_key=RGAPI-51d60eda-093b-4e89-8b9b-f04f7ae8b1f4')
+			.then(async response => sum = await response.json());
+		if (sum.status >= 400) {
+			throw new Error("Bad response from server");
+		}
+
+		let rank = await fetch('https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + sum["id"] + '?api_key=RGAPI-51d60eda-093b-4e89-8b9b-f04f7ae8b1f4');
+		if (rank.status >= 400) {
+			throw new Error("Bad response from server");
+		}
+		rank = await rank.json();
+
+		const puuid = sum['puuid'];
+
+		const con = mysql.createConnection({
+			host: "141.147.19.210",
+			user: "statslol",
+			password: "hCYG6]0*4@iE(yZ)",
+			database: "statslol",
+		});
+
+		let ranksoloq = undefined;
+		let rankflex = undefined;
+
+		if (Object.keys(rank).length > 0) {
+			for (let i = 0; i < Object.keys(rank).length; i++) {
+				if (rank[i]["queueType"] === "RANKED_SOLO_5x5") {
+					ranksoloq = rank[i];
+				}
+				else if (rank[i]["queueType"] === "RANKED_FLEX_SR") {
+					rankflex = rank[i];
+				}
+			}
+		}
+
+		if (ranksoloq === undefined) {
+			ranksoloq = {};
+			ranksoloq['tier'] = 'N/A';
+			ranksoloq['rank'] = '';
+			ranksoloq['leaguePoints'] = 0;
+			ranksoloq['wins'] = 0;
+			ranksoloq['losses'] = 0;
+		}
+
+		if (rankflex === undefined) {
+			rankflex = {};
+			rankflex['tier'] = 'N/A';
+			rankflex['rank'] = '';
+			rankflex['leaguePoints'] = 0;
+			rankflex['wins'] = 0;
+			rankflex['losses'] = 0;
+		}
+
+		const req = 'INSERT INTO accounts VALUES (' + '"' + puuid + '", "' + sum['name'] + '", ' + sum['summonerLevel'] + ', ' + sum['profileIconId'] + ', "' + ranksoloq['tier'] + ' ' + ranksoloq['rank'] + '", ' + ranksoloq['leaguePoints'] + ', ' + ranksoloq['wins'] + ', ' + ranksoloq['losses'] + ', "' + rankflex['tier'] + ' ' + rankflex['rank'] + '", ' + rankflex['leaguePoints'] + ', ' + rankflex['wins'] + ', ' + rankflex['losses'] + ');';
+		await con.connect(function(err) {
+			if (err) throw err;
+			con.query(req, async function(err) {
+				if (err) {
+					if (err.toString().includes("ER_DUP_ENTRY")) {
+						await interaction.reply({ content: "Le compte est déjà enregistré", ephemeral: true });
+					}
+					else {
+						throw err;
+					}
+				}
+				else {
+					await interaction.reply({ content: "Le compte a été ajouté", ephemeral: true });
+				}
+			});
+		});
+	},
+};
