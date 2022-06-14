f4c4a59ac64b64b24df061d6c5821abba5cbfc14
P0SlX <florian.savoure@gmail.com>
2022-06-14 15:34:32 +0200

    Retire le cron

--- events/ready.js
+++ events/ready.js
@@ -7,12 +7,12 @@ module.exports = {
 		console.log(`Connecté en tant que ${client.user.tag}`);
 		global.LOGGER.info(`Connecté en tant que ${client.user.tag}`);
 
-		const cronJob = cron.job('0,30 17 * * 1-5', async () => {
-			const channel = await client.channels.cache.get('667053408636633088');
-			await channel.send({ content: "Finito la journée", files: ['./resources/travail_termine.mp4'] });
-		});
+		// const cronJob = cron.job('0,30 17 * * 1-5', async () => {
+		// 	const channel = await client.channels.cache.get('667053408636633088');
+		// 	await channel.send({ content: "Finito la journée", files: ['./resources/travail_termine.mp4'] });
+		// });
 		client.user.setActivity("des canettes", { type: "STREAMING", url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" });
 
-		cronJob.start();
+		// cronJob.start();
 	},
 };
