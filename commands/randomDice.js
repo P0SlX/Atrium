64b61dea1dda45572e3fdab9ce13ff2240954ceb
Maetis <41517613+Maetis0511@users.noreply.github.com>
2022-10-18 19:17:32 +0200

    modif commande randomDice

--- commands/randomDice.js
+++ commands/randomDice.js
@@ -3,18 +3,17 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('dice')
-        .setDescription('Randint un nombre entre 1 et le nombre de votre choix')
+        .setDescription('Randint un nombre entre 1 et x')
         .addIntegerOption(option => option.setName('nombre').setDescription("Nombre max").setRequired(true)),
     async execute(interaction) {
-        await interaction.reply({ content: "Génération d'un nombre random", ephemeral: true });
         const number = interaction.options.getInteger('nombre');
 
         if (number < 2) {
-            await interaction.editReply({ content: "Le nombre doit être supérieur à 1", ephemeral: false });
+            await interaction.reply({ content: "Le nombre doit être supérieur à 1", ephemeral: false });
         }
         else {
             const randomNumber = Math.floor(Math.random() * number) + 1;
-            await interaction.editReply({ content: `Le nombre random est ${randomNumber}`, ephemeral: false });
+            await interaction.reply({ content: `${randomNumber}`, ephemeral: false });
         }
     },
 };
