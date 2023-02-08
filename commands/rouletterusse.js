cdf55597f1583db98287619071928c0334edaec7
P0SlX <florian.savoure@gmail.com>
2023-02-08 15:40:25 +0100

    Ajout de la roulette russe

---
+++ commands/rouletterusse.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('rouletterusse')
+        .setDescription('Une chance sur 6 de te faire ban'),
+    async execute(interaction) {
+        const random = Math.floor(Math.random() * 6) + 1;
+
+        if (random === 1) {
+            await interaction.reply('Padpo');
+            await interaction.guild.members.ban(interaction.user.id, { reason: 'Roulette russe' });
+        }
+        else {
+            await interaction.reply('La chonce');
+        }
+    },
+};
